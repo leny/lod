808189fc3292f8ed28eb810f2941671218a490bb
leny <info@flatland.be>
2016-01-19 23:42:55 +0100

    Refactor tests

--- test/lod_test.js
+++ test/lod_test.js
@@ -3,12 +3,6 @@
 var lod = require( "../lib/lod.js" ).default;
 
 exports[ "lòd:exceptions" ] = {
-    "lòd throws when arrays are not equal length": function( test ) {
-        test.throws( function() {
-            lod( [ "one", "two", "three" ], [ 1, 0 ] );
-        } );
-        test.done();
-    },
     "lòd throws when order array contains invalid elements": function( test ) {
         test.throws( function() {
             lod( [ "one", "two", "three" ], [ 1, "bar", "foo" ] );
@@ -30,6 +24,8 @@ exports[ "lòd" ] = {
         test.deepEqual( lod( [ "one", "two", "three", "four", "five" ], [ 4, 3, 2, 0, 1 ] ), [ "five", "four", "three", "one", "two" ] );
         test.deepEqual( lod( [ "one", "two", "three", "four", "five" ], [ 3, 2, 4, 1, 0 ] ), [ "four", "three", "five", "two", "one" ] );
         test.deepEqual( lod( [ "one", "two", "three", "four", "five" ], [ 1, 0, 3, 4, 2 ] ), [ "two", "one", "four", "five", "three" ] );
+        test.deepEqual( lod( [ "one", "two", "three", "four", "five" ], [ 1, 0, 4, 2 ] ), [ "two", "one", "five", "three" ] );
+        test.deepEqual( lod( [ "one", "two", "three", "four", "five" ], [ 1, 0, 2, 2, 2, 4 ] ), [ "two", "one", "three", "three", "three", "five" ] );
         test.done();
     }
 };
